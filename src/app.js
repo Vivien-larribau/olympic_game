3c90e033127025b4c4499283e3018077b6c5ee98
Clément DEJEAN <clementdejean99@gmail.com>
2021-12-09 15:08:58 +0100

    Nouvelle structure répretoire public

--- src/app.js
+++ src/app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path')
-const { v4: uuidv4} = require('uuid');
-uuidv4();
+
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index', { name: 'index' });
