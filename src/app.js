4a12704b27eff6e17afbc991a2d68484c691c7b7
Vivien <vivien.larribau@ipilyon.net>
2021-12-10 14:35:47 +0100

    ajout POST pour les athletes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 const path = require('path')
 
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('athletes', path.join(__dirname, '/views/athletes'));
--- src/routers/athlete.router.js
+++ src/routers/athlete.router.js
@@ -2,10 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const AthleteController = require('../controllers/athlete.controller');
+const Athlete = require("../models/athlete.model");
 const athleteController = new AthleteController();
 
-router.get('/athletes/', async (req, res) => {
+router.get('/athletes', async (req, res) => {
     athleteController.listPage(req,res);
+});
+router.post('/athletes/post',async (req, res) => {
+    const paramAthlete = req.body;
+    const athlete = new Athlete({
+        firstname: paramAthlete.firstname,
+        name: paramAthlete.name,
+        region: paramAthlete.region,
+        dicipline: paramAthlete.dicipline,
+        nb_medaille : paramAthlete.nb_medaille
+    });
+    await athlete.save();
+    res.redirect('/api/athletes');
 });
 
 module.exports = router;
