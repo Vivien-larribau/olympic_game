af4e71b6f93c0345e4f8cee737035326531f6629
Vivien <vivien.larribau@ipilyon.net>
2021-12-10 15:41:57 +0100

    ajout post pour le sport

--- src/models/sport.model.js
+++ src/models/sport.model.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const sportSchema = new mongoose.Schema({
     name: String,
@@ -9,7 +9,7 @@ const sportSchema = new mongoose.Schema({
 
     // ... A COMPLETER ...
     // Exemple :
-    //athletes : [{ type: Schema.Types.ObjectId, ref: 'Athlete' }]
+    athletes : [{ type: Schema.Types.ObjectId, ref: 'Athlete' }]
 });
 
 const Sport = mongoose.model('Sport', sportSchema);
--- src/routers/sport.router.js
+++ src/routers/sport.router.js
@@ -2,11 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const SportController = require('../controllers/sport.controller');
+const Sport = require("../models/sport.model");
 const sportController = new SportController();
 
 router.get('/sports', async (req, res) => {
     sportController.listPage(req, res);
     //sportController.list();
 });
+
+router.post('/sports/post', async (req,res)=>{
+    const paramSport = req.body;
+    const sport = new Sport({
+        name: paramSport.name,
+        category: paramSport.category
+    });
+    await sport.save();
+    res.redirect('/api/sports');
+})
+
 
 module.exports = router;
